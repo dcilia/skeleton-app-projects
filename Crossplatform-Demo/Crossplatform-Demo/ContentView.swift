df51ab52f9f4c9ca89f8445d1b21cd003461b60f
David Cilia <mobileproducts@me.com>
2020-07-20 09:24:25 -0400

    Initial commit

---
+++ Crossplatform-Demo/Crossplatform-Demo/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Crossplatform-Demo
+//
+//  Created by David Cilia on 7/20/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Crossplatform-Demo/Crossplatform-Demo/Crossplatform_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crossplatform_DemoApp.swift
+//  Crossplatform-Demo
+//
+//  Created by David Cilia on 7/20/20.
+//
+
+import SwiftUI
+
+@main
+struct Crossplatform_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
